e2bf3fe0d6fc27db7f242225f45332e4cd4eb9e6
omalmaleesha <omalmaleesha03@gmail.com>
2025-08-24 23:16:48 +0530

    add propertyInput DTO class and add change the  controller with DTO

--- src/main/java/com/example/demo/controller/PropertyController.java
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.AppConfig;
+import com.example.demo.dto.PropertyInput;
+import com.example.demo.dto.SearchResponse;
 import com.example.demo.model.Property;
 import com.example.demo.model.PropertyDocument;
-import com.example.demo.service.PropertyService;
+import com.example.demo.service.impl.PropertyServiceImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,19 +15,21 @@ import java.util.List;
 @RequestMapping("/properties")
 public class PropertyController {
 
-    private final PropertyService service;
+    private final PropertyServiceImpl service;
+    private final ModelMapper modelMapper;
 
-    public PropertyController(PropertyService service) {
+    public PropertyController(PropertyServiceImpl service,ModelMapper mapper) {
         this.service = service;
+        this.modelMapper = mapper;
     }
 
     @PostMapping
-    public Property createProperty(@RequestBody Property property) {
-        return service.saveProperty(property);
+    public Property createProperty(@RequestBody PropertyInput propertyInput) {
+        return service.saveProperty(modelMapper.map(propertyInput, Property.class));
     }
 
     @GetMapping("/search")
-    public List<PropertyDocument> search(@RequestParam String q) {
+    public List<SearchResponse> search(@RequestParam String q) {
         return service.searchProperties(q);
     }
 }
---
+++ src/main/java/com/example/demo/dto/PropertyInput.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PropertyInput {
+    private String title;
+    private String description;
+    private Double price;
+}
